85cc9245d7cbce92295f30d5e29352802b730739
EmadAbdoli <Abdoli.mh@gmail.com>
2018-07-14 15:02:14 +0430

    1.keywords added. 2.some code cleaning.

--- src/game/logic.js
+++ src/game/logic.js
@@ -1,4 +1,5 @@
 var utility = require("./utility");
+var keywordsFile = require("./keywords");
 
 // For server
 var Backtory = require("backtory-sdk");
@@ -9,143 +10,12 @@ var waitUntil = require("./myLibs/wait-until");
 //var waitUntil = require("./../../api/node_modules/wait-until");
 //var fs = require('fs');
 //Backtory.setConfigFileLocation("C:/Users/Emad/Downloads/Compressed/Ubuntu.1604.2017.711.0_v1/rootfs/home/Emad/Server_backtory/src/backtory_config.json");
-
-var topics = 
-[
-    "Travel",
-    "College Life",
-    "Socializing",
-    "Employment",
-    "Voting",
-    "Daily Life",
-    "Buying a Car",
-    "In a New Neighborhood",
-    "At a Hotel",
-    "Small Talk",
-    "Driving",
-    "At the Bank",
-    "At the Library",
-    "Renting an Apartment",
-    "Buying a House",
-    "Unemployment",
-    "Health",
-    "Crime",
-    "Shopping",
-    "Taking the Bus",
-    "Transferring to a University",
-    "Food",
-    "At a Restaurant",
-    "Dating",
-    "Selling a House"
-];
-var gameTypesIds = 
-{
-    "1" : "5ae58c6f77e6b100010d11c5",
-    "2" : "5ae58c21790c0c0001d2387b"
-}
 var playerCounts = 3;
 
-var gameTypeIdFinder = function(matchName)
-{
-    var arr = matchName.split("Matching");
-    return arr[1];
-}
 
-var getUserPid = function(participant)
-{
-    var metastring = participant.metaData;
-    var myMetadata = JSON.parse(metastring);
-
-    return myMetadata[1];
-}
-
-var calcTopic = function (choices) // Designed for 3 Topics
-{
-    var res = [0,0,0];
-    for (var uid in choices)
-    {
-        res[choices[uid]] = res[choices[uid]] + 1;
-    }
-
-    var result = 0;
-    if(res[1] > res[0]) result = 1;
-    if(res[1] == res[0]) result = utility.getRandomInt(2);
-    if(res[2] > res[result]) result = 2;
-    if(res[2] == res[result]) 
-    {
-        if (utility.getRandomInt(2) == 0)
-            result = 2;
-    }
-
-    return result;
-}
-
-var setGameRelations = function(gid, participants)
-{
-    var Game = Backtory.Object.extend("games");
-    var game = new Game();
-    var gamePlayerRelation = game.relation("players");
-    game.set("_id", gid);
-
-    var Player = Backtory.Object.extend("players");
-    for(var i = 0; i < participants.length; i++)
-    {
-        var tempPlayer = new Player();
-        var pid = getUserPid(participants[i]);
-
-        tempPlayer.set("_id", pid);
-        gamePlayerRelation.add(tempPlayer);
-    }
-
-    game.save({
-        success:function(game){}
-    });
-}
-
-var setGameTypeRelations = function(game, gameTypeId, matchId)
-{
-    var GameType = Backtory.Object.extend("gameType");
-
-    var myGameType = new GameType();
-    myGameType.set("_id", gameTypesIds[gameTypeId]);
-    var gamesRelation = myGameType.relation("games");
-    gamesRelation.add(game);
-
-    myGameType.save({
-        success:function(gameType)
-        {
-            game.set("gameType", gameType);
-            game.set("tableValue", 1);
-            game.set("challengeId",matchId);
-
-            game.save();
-        }
-    });
-}
-
-var setPlayersRelations = function(gid, participants)
-{
-    var Game = Backtory.Object.extend("games");
-    var Player = Backtory.Object.extend("players");
-    
-    var game = new Game();
-    game.set("_id", gid);
-
-    for(var i = 0; i < participants.length; i++)
-    {
-        var tempPlayer = new Player();
-        var pid = getUserPid(participants[i]);
-
-        tempPlayer.set("_id", pid);
-        var pGames = tempPlayer.relation("games");
-        pGames.add(game);
-        tempPlayer.save({
-            success:function(tempPlayer){}
-        });        
-    }
-
-    console.log("players' games saved");
-}
+//********************************************************************************************************** */
+//********************************************************************************************************** */
+//********************************************************************************************************** */
 
 // on MatchFound we call this function
 // For server
@@ -156,12 +26,12 @@ exports.onMatchFoundController = function (requestBody, context) {
     var matchId = requestBody.realtimeChallengeId;
     var participants = requestBody.participants;
     var matchmakingName = requestBody.matchmakingName;
-    var gameTypeId = gameTypeIdFinder(matchmakingName);
+    var gameTypeId = utility.gameTypeIdFinder(matchmakingName);
 
     var Game = Backtory.Object.extend("games");
     var game = new Game();
 
-    setGameTypeRelations(game, gameTypeId, matchId);
+    utility.setGameTypeRelations(game, gameTypeId, matchId);
  
     waitUntil()
         .interval(100)
@@ -170,7 +40,7 @@ exports.onMatchFoundController = function (requestBody, context) {
             return (game.get("_id") != undefined ? true : false);
         })
         .done(function(result) {
-            setGameRelations(game.get("_id"), participants);
+            utility.setGameRelations(game.get("_id"), participants);
     });
 
     waitUntil()
@@ -180,7 +50,7 @@ exports.onMatchFoundController = function (requestBody, context) {
         return (true ? true : false);
     })
     .done(function(result) {
-        setPlayersRelations(game.get("_id"), participants);
+        utility.setPlayersRelations(game.get("_id"), participants);
     });
 
     var rndNumbers = [];
@@ -200,15 +70,19 @@ exports.onMatchFoundController = function (requestBody, context) {
 
     var selectedTopics = 
     [
-        topics[rndNumbers[0]],
-        topics[rndNumbers[1]],
-        topics[rndNumbers[2]],
+        keywordsFile.topics[rndNumbers[0]],
+        keywordsFile.topics[rndNumbers[1]],
+        keywordsFile.topics[rndNumbers[2]],
     ];
     
     // Return selected topics for this challenge
     // Todo: for push code on server uncomment this line
     context.succeed(JSON.stringify(selectedTopics));
 };
+
+//********************************************************************************************************** */
+//********************************************************************************************************** */
+//********************************************************************************************************** */
 
 // For server
 exports.gameEventController = function (requestBody, context) {
@@ -269,7 +143,7 @@ exports.gameEventController = function (requestBody, context) {
 
                 if (Object.keys(props.choices).length == playerCounts)
                 {
-                    var topicIndex = calcTopic(props.choices);
+                    var topicIndex = utility.calcTopic(props.choices);
                     props["topic"]=  props.topics[topicIndex];
                     result = {operation: 'subjectSelected', 
                                 userId: userId, choice: requestBody.data.choice, topic : topicIndex};
@@ -294,6 +168,10 @@ exports.gameEventController = function (requestBody, context) {
     // For Server
     context.succeed(tResult);
 };
+
+//********************************************************************************************************** */
+//********************************************************************************************************** */
+//********************************************************************************************************** */
 
 var reqbody = {
     "realtimeChallengeId": "123123edf456",
--- src/game/utility.js
+++ src/game/utility.js
@@ -1,3 +1,143 @@
+// For Server
+var Backtory = require("backtory-sdk");
+// For Local
+//var Backtory = require("./../../api/node_modules/backtory-sdk");
+
+/********************************************************************************** */
+/********************************************************************************** */
+
+var gameTypesIds = 
+{
+    "1" : "5ae58c6f77e6b100010d11c5",
+    "2" : "5ae58c21790c0c0001d2387b"
+}
+
+/********************************************************************************** */
+/********************************************************************************** */
+
 exports.getRandomInt = function (max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+/********************************************************************************** */
+/********************************************************************************** */
+
+exports.gameTypeIdFinder = function(matchName)
+{
+    var arr = matchName.split("Matching");
+    return arr[1];
+}
+
+/********************************************************************************** */
+/********************************************************************************** */
+
+exports.getUserPid = function(participant)
+{
+    var metastring = participant.metaData;
+    var myMetadata = JSON.parse(metastring);
+
+    return myMetadata[1];
+}
+
+/********************************************************************************** */
+/********************************************************************************** */
+
+exports.calcTopic = function (choices) // Designed for 3 Topics
+{
+    var res = [0,0,0];
+    for (var uid in choices)
+    {
+        res[choices[uid]] = res[choices[uid]] + 1;
+    }
+
+    var result = 0;
+    if(res[1] > res[0]) result = 1;
+    if(res[1] == res[0]) result = utility.getRandomInt(2);
+    if(res[2] > res[result]) result = 2;
+    if(res[2] == res[result]) 
+    {
+        if (utility.getRandomInt(2) == 0)
+            result = 2;
+    }
+
+    return result;
+}
+
+/********************************************************************************** */
+/********************************************************************************** */
+
+exports.setGameRelations = function(gid, participants)
+{
+    var Game = Backtory.Object.extend("games");
+    var game = new Game();
+    var gamePlayerRelation = game.relation("players");
+    game.set("_id", gid);
+
+    var Player = Backtory.Object.extend("players");
+    for(var i = 0; i < participants.length; i++)
+    {
+        var tempPlayer = new Player();
+        var pid = this.getUserPid(participants[i]);
+
+        tempPlayer.set("_id", pid);
+        gamePlayerRelation.add(tempPlayer);
+    }
+
+    game.save({
+        success:function(game){}
+    });
+}
+
+/********************************************************************************** */
+/********************************************************************************** */
+
+exports.setGameTypeRelations = function(game, gameTypeId, matchId)
+{
+    var GameType = Backtory.Object.extend("gameType");
+
+    var myGameType = new GameType();
+    myGameType.set("_id", gameTypesIds[gameTypeId]);
+    var gamesRelation = myGameType.relation("games");
+    gamesRelation.add(game);
+
+    myGameType.save({
+        success:function(gameType)
+        {
+            game.set("gameType", gameType);
+            game.set("tableValue", 1);
+            game.set("challengeId",matchId);
+
+            game.save();
+        }
+    });
+}
+
+/********************************************************************************** */
+/********************************************************************************** */
+
+exports.setPlayersRelations = function(gid, participants)
+{
+    var Game = Backtory.Object.extend("games");
+    var Player = Backtory.Object.extend("players");
+    
+    var game = new Game();
+    game.set("_id", gid);
+
+    for(var i = 0; i < participants.length; i++)
+    {
+        var tempPlayer = new Player();
+        var pid = this.getUserPid(participants[i]);
+
+        tempPlayer.set("_id", pid);
+        var pGames = tempPlayer.relation("games");
+        pGames.add(game);
+        tempPlayer.save({
+            success:function(tempPlayer){}
+        });        
+    }
+
+    console.log("players' games saved");
+}
+
+/********************************************************************************** */
+/********************************************************************************** */
